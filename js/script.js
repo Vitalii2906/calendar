fa728c9228a5d4bd5ad95b1ad1c5dd5025cee84a
Vitaliy2906 <fahivchik1@gmail.com>
2020-10-07 11:38:44 +0300

    finish create app

--- js/script.js
+++ js/script.js
@@ -1,45 +1,96 @@
-function calendar(id, year, month) {
-  let link = "http://api.tvmaze.com/schedule?country=US&date=2014-12-01";
-  var Dlast = new Date(year,month+1,0).getDate(),
-      D = new Date(year,month,Dlast),
-      DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
-      DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
-      calendar = '<tr>',
-      month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
-  if (DNfirst != 0) {
-    for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
-  }else{
-    for(var  i = 0; i < 6; i++) calendar += '<td>';
-  }
-  for(var  i = 1; i <= Dlast; i++) {
-    if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-      calendar += '<td class="today">' + i;
-    }else{
-      calendar += `<td> <a href=${link}>${i}</a>`;
-    }
-    if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
-      calendar += '<tr>';
-    }
-  }
-  for(var  i = DNlast; i < 7; i++) calendar += '<td> ';
-  document.querySelector('#'+id+' tbody').innerHTML = calendar;
-  document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()];
-  document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
-  document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
-  if (document.querySelectorAll('#'+id+' tbody tr').length < 5) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
-      document.querySelector('#'+id+' tbody').innerHTML += `<tr><td> <td> <td> <td> <td> <td> <td> ` + link;
-  }
+const request = 'http://api.tvmaze.com/schedule?country=US&date='
+
+$('#datepicker').datepicker({
+  showOtherMonths: true,
+	dateFormat : "yy-mm-dd",
+	minDate: new Date($('#hiddendelivdate').val()),
+	monthNames : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+  dayNamesMin : ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+  onSelect: function (date) {
+    dataProcessing(date, request)
+    /*let option = {year: 'numeric', month: 'long', day: 'numeric'};
+    let thisDay = new Date(date).toLocaleString('ru', option);
+    let newDate = new Date(date);
+    newDate = newDate.setDate(newDate.getDate()+1);
+    let nextDay = new Date(newDate).toLocaleString('ru', option);
+
+    $('#thisDay').append(`<h4>${thisDay}</h4>`)
+    $('#nextDay').append(`<h4>${nextDay}</h4>`)
+    */
+}
+});
+var home = $("#home")
+var catalog = $("#catalog")
+var back = $("#back")
+
+$("tbody td").on("click", function(event){
+  home.hide();
+  catalog.show();
+  back.show();
+
+
   
-}
-calendar("calendar", new Date().getFullYear(), new Date().getMonth());
-// переключатель минус месяц
-document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
-  calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)-1);
-}
-// переключатель плюс месяц
-document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
-  calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month)+1);
-}
+})
+back.on("click", function(event){
+  catalog.hide();
+  home.show();
+  back.hide();
+  window.location.reload();
+})
 
-//ссылка на афишу
 
+function dataProcessing(date, request){
+  let option = {year: 'numeric', month: 'long', day: 'numeric'};
+    let thisDay = new Date(date).toLocaleString('ru', option);
+    let newDate = new Date(date);
+    newDate = newDate.setDate(newDate.getDate()+1);
+    let nextDay = new Date(newDate).toLocaleString('ru', option);
+
+    $('#thisDay').append(`<h4>${thisDay}</h4>`)
+    $('#nextDay').append(`<h4>${nextDay}</h4>`)
+
+    let newNextDay = new Date(newDate).toLocaleString('en-CA', {year: 'numeric', month: 'numeric', day: 'numeric'});
+    let requestThisDay = request + date;
+    let requestNextDay = request + newNextDay;
+
+    $.get(requestThisDay, function(data) {
+      $.each(data, function(index, value){
+        
+        if(index === 4){
+          return false
+        }
+
+        $('#thisDay').append(`<div>
+          <h5>${value.show.name}</h5>
+          <img src=${value.show.image.medium} alt='picture' style='width:80px'>
+          <span>${value.season}</span>
+          <span>${value.number}</span>
+        </div>`)
+        
+      })
+        console.log(data)
+        
+        
+    }, "json" );
+
+
+    $.get(requestNextDay, function(data) {
+      $.each(data, function(index, value){
+        
+        if(index === 4){
+          return false
+        }
+
+        $('#nextDay').append(`<div>
+          <h5>${value.show.name}</h5>
+          <img src=${value.show.image.medium} alt='picture' style='width:80px'>
+          <span>${value.season}</span>
+          <span>${value.number}</span>
+        </div>`)
+        
+      })
+        console.log(data)
+        
+        
+    }, "json" );
+}
